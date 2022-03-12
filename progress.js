565342fb13d684693cdd552af67a16b1ce2b1238
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2022-03-12 11:44:10 +0100

    In progress

---
+++ progress.js
@@ -0,0 +1,13 @@
+let pro1 = document.getElementById("pro1");
+let pro2 = document.getElementById("pro2");
+let pro3 = document.getElementById("pro3");
+let pro4 = document.getElementById("pro4");
+let pro5 = document.getElementById("pro5");
+
+function addUp() {
+  pro1.value = 100;
+  pro2.value = 100;
+  pro3.value = 100;
+  pro4.value = 100;
+  pro5.value = 100;
+}
